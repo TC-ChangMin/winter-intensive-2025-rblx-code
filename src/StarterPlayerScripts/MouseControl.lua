6a3eef136967f2e66967b9c7c145cf44db74f1fd
TC-ChangMin <nickmn2006@gmail.com>
2025-01-15 18:02:33 -0800

    added mouse on click detection. changes objects to red for visiualization --temporary--

--- src/StarterPlayerScripts/MouseControl.lua
+++ src/StarterPlayerScripts/MouseControl.lua
@@ -2,6 +2,7 @@ local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
 local camera = workspace.CurrentCamera
 
+local item = nil
 
 local function MouseRaycast()
 	local mousePosition = UserInputService:GetMouseLocation()
@@ -11,9 +12,20 @@ local function MouseRaycast()
 	return raycastResult
 end
 
+UserInputService.InputBegan:Connect(function(input, processed)
+	if processed then
+		return
+	end
+	if input.UserInputType == Enum.UserInputType.MouseButton1 then
+		if item:IsA("BasePart") then
+			item.Color = Color3.new(1,0,0) -- this is to visualize what is being clicked, will remove later
+		end
+	end
+end)
+
 RunService.RenderStepped:Connect(function()
 	local result = MouseRaycast()
 	if result and result.Instance then
-		print(result.Instance.Name)
+		item = result.Instance
 	end
 end)
