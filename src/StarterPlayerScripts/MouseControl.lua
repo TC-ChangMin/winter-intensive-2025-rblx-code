8533c0cf9f7b122bc61493e046ea06a134f5864e
TC-ChangMin <nickmn2006@gmail.com>
2025-01-15 17:48:33 -0800

    loop added for mouse tracking

--- src/StarterPlayerScripts/MouseControl.lua
+++ src/StarterPlayerScripts/MouseControl.lua
@@ -1,3 +1,4 @@
+local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
 local camera = workspace.CurrentCamera
 
@@ -5,7 +6,14 @@ local camera = workspace.CurrentCamera
 local function MouseRaycast()
 	local mousePosition = UserInputService:GetMouseLocation()
 	local mouseRay = camera:ViewportPointToRay(mousePosition.X, mousePosition.Y)
-
 	local raycastResult = workspace:Raycast(mouseRay.Origin, mouseRay.Direction * 1000)
-
+	
+	return raycastResult
 end
+
+RunService.RenderStepped:Connect(function()
+	local result = MouseRaycast()
+	if result and result.Instance then
+		print(result.Instance.Name)
+	end
+end)
