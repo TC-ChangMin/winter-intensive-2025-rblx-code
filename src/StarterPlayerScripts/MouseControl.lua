828f7d593f102f57300f1d8196a2cea61b8539b2
TC-ChangMin <nickmn2006@gmail.com>
2025-01-16 11:59:52 -0800

    mouse raycasting added. fixed recursion glitch

--- src/StarterPlayerScripts/MouseControl.lua
+++ src/StarterPlayerScripts/MouseControl.lua
@@ -1,31 +1,57 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
+
+local towers = ReplicatedStorage:WaitForChild("Towers")
+
 local camera = workspace.CurrentCamera
+local gui = script.Parent
+local towerToSpawn = nil
 
-local item = nil
-
-local function MouseRaycast()
+local function MouseRaycast(blacklist)
 	local mousePosition = UserInputService:GetMouseLocation()
 	local mouseRay = camera:ViewportPointToRay(mousePosition.X, mousePosition.Y)
-	local raycastResult = workspace:Raycast(mouseRay.Origin, mouseRay.Direction * 1000)
+	local raycastParams = RaycastParams.new()
+	
+	raycastParams.FilterType = Enum.RaycastFilterType.Exclude
+	raycastParams.FilterDescendantsInstances = blacklist
+	
+	local raycastResult = workspace:Raycast(mouseRay.Origin, mouseRay.Direction * 1000, raycastParams)
 	
 	return raycastResult
 end
+
+local function AddTempTower(name)
+	local towerExists = towers:FindFirstChild(name)
+	if towerExists then
+		towerToSpawn = towerExists:Clone()
+		towerToSpawn.Parent = workspace.Towers
+	end
+end
+
+gui.Tower1Button.Activated:Connect(function()
+	AddTempTower("Tower1")
+end)
 
 UserInputService.InputBegan:Connect(function(input, processed)
 	if processed then
 		return
 	end
 	if input.UserInputType == Enum.UserInputType.MouseButton1 then
-		if item:IsA("BasePart") then
-			item.Color = Color3.new(1,0,0) -- this is to visualize what is being clicked, will remove later
-		end
+		
 	end
 end)
 
 RunService.RenderStepped:Connect(function()
-	local result = MouseRaycast()
-	if result and result.Instance then
-		item = result.Instance
+	if towerToSpawn then
+		local result = MouseRaycast({towerToSpawn})
+		if result and result.Instance then
+			local x = result.Position.X
+			local y = result.Position.Y
+			local z = result.Position.Z
+			
+			local cframe = CFrame.new(x,y,z)
+			towerToSpawn:SetPrimaryPartCFrame(cframe)
+		end
 	end
 end)
