ac089e169e3a70524255434a82aee8386be96b26
TC-ChangMin <nickmn2006@gmail.com>
2025-01-16 17:49:08 -0800

    prevented towers from being placed on road. prevented towers from being spammed. added tower rotation with r key on click

--- src/StarterPlayerScripts/MouseControl.lua
+++ src/StarterPlayerScripts/MouseControl.lua
@@ -3,11 +3,15 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
 
+local events = ReplicatedStorage:WaitForChild("Events")
 local towers = ReplicatedStorage:WaitForChild("Towers")
-
+local spawnTower = events:WaitForChild("SpawnTower")
 local camera = workspace.CurrentCamera
 local gui = script.Parent
+
 local towerToSpawn = nil
+local canPlaceTower = false
+local rotateTower = 0
 
 local function MouseRaycast(blacklist)
 	local mousePosition = UserInputService:GetMouseLocation()
@@ -22,9 +26,18 @@ local function MouseRaycast(blacklist)
 	return raycastResult
 end
 
+local function removeTempTower()
+	if towerToSpawn then
+		towerToSpawn:Destroy()
+		towerToSpawn = nil
+	end
+end
+
+
 local function AddTempTower(name)
 	local towerExists = towers:FindFirstChild(name)
 	if towerExists then
+		removeTempTower()
 		towerToSpawn = towerExists:Clone()
 		towerToSpawn.Parent = workspace.Towers
 		
@@ -37,6 +50,14 @@ local function AddTempTower(name)
 	end
 end
 
+local function ColorTempTower(color)
+	for i, object in ipairs(towerToSpawn:GetDescendants()) do
+		if object:IsA("BasePart") then
+			object.Color = color
+		end
+	end
+end
+
 gui.Tower1Button.Activated:Connect(function()
 	AddTempTower("Tower1")
 end)
@@ -45,8 +66,16 @@ UserInputService.InputBegan:Connect(function(input, processed)
 	if processed then
 		return
 	end
-	if input.UserInputType == Enum.UserInputType.MouseButton1 then
-		
+	if towerToSpawn then
+		if input.UserInputType == Enum.UserInputType.MouseButton1 then
+			if canPlaceTower then
+			spawnTower:FireServer(towerToSpawn.Name, towerToSpawn.PrimaryPart.CFrame)
+			removeTempTower()
+			end
+		else if input.KeyCode == Enum.KeyCode.R then
+				rotateTower += 90
+			end
+		end
 	end
 end)
 
@@ -54,11 +83,19 @@ RunService.RenderStepped:Connect(function()
 	if towerToSpawn then
 		local result = MouseRaycast({towerToSpawn})
 		if result and result.Instance then
+			if result.Instance.Parent.Name == "PlaceableArea" then
+				canPlaceTower = true
+				ColorTempTower(Color3.new(0,1,0))
+			else
+				canPlaceTower = false
+				ColorTempTower(Color3.new(1,0,0))
+			end
+			
 			local x = result.Position.X
 			local y = result.Position.Y + towerToSpawn["Left Leg"].Size.Y + (towerToSpawn.PrimaryPart.Size.Y / 2)
 			local z = result.Position.Z
 			
-			local cframe = CFrame.new(x,y,z)
+			local cframe = CFrame.new(x,y,z) * CFrame.Angles(0, math.rad(rotateTower), 0)
 			towerToSpawn:SetPrimaryPartCFrame(cframe)
 		end
 	end
