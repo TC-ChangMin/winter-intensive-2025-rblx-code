c51ed887e4a4b4b10cab487729b7dc39392abde7
TC-ChangMin <nickmn2006@gmail.com>
2025-01-16 21:07:24 -0800

    incorporated the TowerTesting script into the Tower script. towers now attack after placement

--- src/ServerScriptService/Tower.lua
+++ src/ServerScriptService/Tower.lua
@@ -6,6 +6,34 @@ local events = ReplicatedStorage:WaitForChild("Events")
 local spawnTower = events:WaitForChild("SpawnTower")
 local tower = {}
 
+function FindNearestTarget(newTower)
+	local maxDistance = 30 --edit for tower range
+	local nearestTarget = nil
+
+	for i, target in ipairs(workspace.Mobs:GetChildren()) do
+		local distance = (target.HumanoidRootPart.Position - newTower.HumanoidRootPart.Position).Magnitude
+		print(target.Name, distance)
+
+		if distance <= maxDistance then
+			print(target.Name, "target found at", distance, "studs away")
+			nearestTarget = target
+			maxDistance = distance
+		end
+	end
+
+	return nearestTarget
+end
+
+function tower.Attack(newTower)
+	local target = FindNearestTarget(newTower)
+	if target then
+		target.Humanoid:TakeDamage(25)
+	end
+
+	task.wait(1)
+	tower.Attack(newTower) --creates an inf recursive
+end
+
 function tower.Spawn(player, name, cframe)
 	local towerExists = ReplicatedStorage.Towers:FindFirstChild(name)
 	
@@ -20,6 +48,8 @@ function tower.Spawn(player, name, cframe)
 				object.CollisionGroup = "Tower"
 			end
 		end
+		
+		coroutine.wrap(tower.Attack)(newTower)
 	else
 		warn("Tower Does Not Exist -->", name)
 	end
